ef54ebe25c9493ed184c540de7c1d325aa3b02bf
Kamo98 <avanesjan.k@gmail.com>
2020-05-03 22:42:31 +0700

    init

---
+++ src/main/java/ru/vkr/vkr/controller/TestController.java
@@ -0,0 +1,43 @@
+package ru.vkr.vkr.controller;
+
+
+import edu.csus.ecs.pc2.api.ITeam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.vkr.vkr.contest.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/genTeams")
+    public String genTeams(Model model) {
+        Test test = new Test();
+
+        List<String> resultsGen = test.generate_teams();
+        test.disconnect();
+
+        model.addAttribute("resultsGen", resultsGen);
+        return "gen";
+    }
+
+    @GetMapping("/print")
+    public String ptint(Model model) {
+        Test test = new Test();
+        ITeam[] teams_ = test.get_all_teams();
+        List<String> teams = new ArrayList<>();
+        for (ITeam team: teams_) {
+            teams.add(team.getDisplayName());
+        }
+
+        model.addAttribute("teams", teams);
+        //test.generate_teams();
+        test.disconnect();
+
+        return "print";
+    }
+}
