f7d3b95411cbff347b3fc45eab1f2f5a1f77ad6c
Kamo98 <avanesjan.k@gmail.com>
2020-05-07 17:19:49 +0700

    Начал делать сохранение курса

--- src/main/java/ru/vkr/vkr/entity/Course.java
+++ src/main/java/ru/vkr/vkr/entity/Course.java
@@ -1,12 +1,20 @@
 package ru.vkr.vkr.entity;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name="t_course")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @NotNull
+    @Column(nullable = false)
     private String name;
+
 
 
     public String getName() {
